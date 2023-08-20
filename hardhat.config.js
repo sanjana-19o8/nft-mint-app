7814e591a36a21190541a91b8f1d474c74596d9d
sanjana-19o8 <sanjanavajrapu19@gmail.com>
2023-08-21 00:58:37 +0530

    finally

--- hardhat.config.js
+++ hardhat.config.js
@@ -9,20 +9,9 @@ module.exports = {
     artifacts: './artifacts',
   },
   networks: {
-    // hardhat: {},
     sepolia: {
-      url: "https://eth-sepolia.g.alchemy.com/v2/Px-VVT9EoZ-TCZtki8ioxLD2kVDuZJJA",
+      url: process.env.RPC_URL,
       accounts: [process.env.PRIVATE_KEY],
-    },
-    // mumbai: {
-      //   url: 'https://polygon-mumbai.g.alchemy.com/v2/fDDUXgGKp4JNDJVedU0P4z-hWiprUstr',
-      //   accounts: [process.env.PRIVATE_KEY]
-      // }
-    },
-    // alchemy: {
-    //   apiKey: process.env.ALCHEMY_API_KEY,
-    // },
-    etherscan: {
-      apiKey: process.env.ETHERSCAN_API_KEY,
     }
+  }
 };
--- test/ArcadeCats.js
+++ test/ArcadeCats.js
@@ -1,26 +1,24 @@
-const {expect} = require('chai');
+const { expect } = require('chai');
 const ethers = require("hardhat");
-const {it} = require('mocha');
 
-const { getAddress } = require('ethers').utils;
+describe("ArcadeCats", function () {
+  it("Should mint and transfer an NFT to a valid address", async function () {
+    const AcradeCats = await hre.ethers.deployContract("ArcadeCats");
+    await AcradeCats.waitForDeployment();
 
+    console.log('Contract deployed to ', AcradeCats.target);
 
-describe("ArcadeCats", function() {
-    it("Should mint and transfer an NFT to a valid address", async function() {
-        const AcradeCats = await hre.ethers.deployContract("ArcadeCats");
-        await AcradeCats.waitForDeployment();
+    const recipient = '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266';
+    const metadataURI = 'QmWWTdhvY3g493K15a5VjSTTnaxmDPf1jsLt4iJigzD71E';
 
-        const recipient = '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266';
-        const metadataURI = 'QmWWTdhvY3g493K15a5VjSTTnaxmDPf1jsLt4iJigzD71E';
+    let balance = await AcradeCats.balanceOf(recipient);
+    expect(balance).to.equal(0);
 
-        let balance = await AcradeCats.balanceOf(recipient);
-        expect(balance).to.equal(0);
+    const newlyMintedToken = await AcradeCats.payToMint(recipient, metadataURI, { value: hre.ethers.parseEther('0.51') });
 
-        const newlyMintedToken = await AcradeCats.payToMint(recipient, metadataURI, { value: ethers.utils.parseEther('0.51')});
+    balance = await AcradeCats.balanceOf(recipient);
 
-        balance = await AcradeCats.balanceOf(recipient);
-        
-        expect(balance).to.equal(1);
-        expect(await AcradeCats.isContentOwned(metadataURI)).to.equal(true);
-    })
+    expect(balance).to.equal(1);
+    expect(await AcradeCats.isOwned(metadataURI)).to.equal(false);
+  })
 })
