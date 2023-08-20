a475c1df131c50447864e3a4576d37ba6c443c1b
sanjana-19o8 <sanjanavajrapu19@gmail.com>
2023-08-20 19:20:58 +0530

    asdf

--- test/ArcadeCats.js
+++ test/ArcadeCats.js
@@ -1,5 +1,5 @@
 const {expect} = require('chai');
-const hre = require("hardhat");
+const ethers = require("hardhat");
 const {it} = require('mocha');
 
 const { getAddress } = require('ethers').utils;
@@ -7,23 +7,20 @@ const { getAddress } = require('ethers').utils;
 
 describe("ArcadeCats", function() {
     it("Should mint and transfer an NFT to a valid address", async function() {
-        const AcradeCats = await hre.ethers.getContractFactory("ArcadeCats");
-        const acradecats = await AcradeCats.deploy();
-        await acradecats.deployed();
-        
-        console.log('Contract deployed to ', getAddress(arcacdecats));
+        const AcradeCats = await hre.ethers.deployContract("ArcadeCats");
+        await AcradeCats.waitForDeployment();
 
         const recipient = '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266';
         const metadataURI = 'QmWWTdhvY3g493K15a5VjSTTnaxmDPf1jsLt4iJigzD71E';
 
-        let balance = await acradecats.balanceOf(recipient);
+        let balance = await AcradeCats.balanceOf(recipient);
         expect(balance).to.equal(0);
 
-        const newlyMintedToken = await acradecats.payToMint(recipient, metadataURI, { value: ethers.utils.parseEther('0.51')});
+        const newlyMintedToken = await AcradeCats.payToMint(recipient, metadataURI, { value: ethers.utils.parseEther('0.51')});
 
-        balance = await acradecats.balanceOf(recipient);
+        balance = await AcradeCats.balanceOf(recipient);
         
         expect(balance).to.equal(1);
-        expect(await firedguys.isContentOwned(metadataURI)).to.equal(true);
+        expect(await AcradeCats.isContentOwned(metadataURI)).to.equal(true);
     })
 })
